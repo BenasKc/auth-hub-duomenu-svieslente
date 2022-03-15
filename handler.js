23b1be0eca14924ed9a9d0b1da7eaaa4db97968a
BenasKc <benas.kucinskas.org@gmail.com>
2022-03-15 23:03:19 +0200

    msg

--- handler.js
+++ handler.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const parser = require('./parser.js')
 const { url } = require("inspector");
-var port = process.env.PORT || 80
+const port = process.env.PORT || 5000
 
 const server = http.createServer((req,res)=>{
     if(req.url === '/create'){
