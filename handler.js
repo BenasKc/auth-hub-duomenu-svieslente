9a67f3b3730e8999e7bb4d3135ae65b75cfdd780
BenasKc <benas.kucinskas.org@gmail.com>
2022-04-27 22:39:28 +0300

    MySQL update

--- handler.js
+++ handler.js
@@ -1,93 +1,94 @@
-const http = require("http");
-const fs = require("fs");
-const parser = require('./parser.js')
-const { url } = require("inspector");
-const port = 8000//process.env.PORT || 5000
+const port = 8009;
+const host = 'localhost';
+const { readFile } = require('fs');
+const crypto = require('crypto');
+const http = require('http');
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'administratorius',
+    password: '10KyS92K94414dG',
+    database: 'Duomenu_Svieslente'
+});
+// routes to fix: /create /fetchprofile /checklogin /create_new_chart
+const requestListener = function (req, res) {
+    if(req.url === '/checklogin'){
+        req.on('data', (chunk)=>{
+            
+            var data_obj = JSON.parse(chunk.toString());
+            data_obj[0] = data_obj[0].replace(/[^\w\s]/gi, '');
+            data_obj[1] = data_obj[1].replace(/[^\w\s]/gi, '');
 
-const server = http.createServer((req,res)=>{
-    if(req.url === '/create'){
-        var status;
-        req.on('data', chunk => {
-            var item = chunk.toString().split('|');
-            status = parser.create_acc(item[0], item[1], item[2], item[3], item[4])
-            res.writeHead(200, {'Content-Type':'text/plain'});
-            res.write(JSON.stringify(status));
-            res.end();
-          })
+
+            if(data_obj[0].length > 63)res.end("Too long username");
+            else{
+                connection.query(`CALL Duomenu_Svieslente.sp_Check_Login('${data_obj[0]}')`, function(error, result){
+                    if(error) throw error;
+                    console.log("here" + result);
+                    if(result && result[0].length === 0){
+
+
+                        res.write('-1');
+                        res.end();
+                    }
+                    else{
+                        var hash = crypto.pbkdf2Sync(data_obj[1],
+                            result[0][0].Hash, 1000, 64, `sha512`).toString(`hex`);
+                        if(hash === result[0][0].Password){
+                            connection.query(`CALL Duomenu_Svieslente.sp_Init_Session('${data_obj[0]}')`, function(er, reslt){
+                                if(er) throw er;
+                                res.write(reslt[0][0].uid);
+                                res.end();
+                            })
+                        }
+                        else{
+                            console.log('Incorrect login');
+                            res.write('-1');
+                            res.end();
+                        }
+                    }
+                })
+            }
+        })
     }
-    else if(req.url === '/checklogin'){
-        var status;
-        req.on('data', chunk => {
-            var item = chunk.toString().split('|');
-            status = parser.check_login(item[0], item[1], 'name')
-            res.writeHead(200, {'Content-Type':'text/plain'});
-            res.write(JSON.stringify(status));
-            res.end();
-          })
-          
+    else if(req.url === '/check_token'){
+        req.on('data', (chunk)=>{
+                if(chunk.toString().replace(/[^-\w\s]/gi, '').length > 0){
+                    console.log(chunk.toString().replace(/[^-\w\s]/gi, ''));
+                    connection.query(`CALL Duomenu_Svieslente.sp_Check_Token('${chunk.toString().replace(/[^-\w\s]/gi, '')}')`, function(err, result){
+                    if(err)throw err;
+                    if(result[0][0].stat === 'true'){
+                        res.write('true');
+                        res.end();
+                    }
+                    else{
+                        res.write('false');
+                        res.end();
+                    }
+                    })
+                }
+                
+            
+            
+        })
     }
-    else if(req.url === '/fetchprofile'){
-        var status;
-        req.on('data', chunk => {
-            var item = chunk.toString().split('|');
-            status = parser.fetch_profile(item[0].slice(1, -1)); // id
-            res.writeHead(200, {'Content-Type':'text/plain'});
-            res.write(JSON.stringify(status));
-            res.end();
-          })
-          
-    }
-    else if(req.url === '/create_chart'){
-        var status;
-        req.on('data', chunk => {
-            var object = JSON.parse(chunk.toString());
-            status = parser.create_chr(object, (cb)=>{
-                res.writeHead(200, {'Content-Type':'text/plain'});
-                res.write(JSON.stringify(cb));
+    else if(req.url === '/create'){
+        req.on('data', (chunk)=>{
+            var data = JSON.parse((chunk).toString());
+            var salt = crypto.randomBytes(16).toString('hex');
+            data[1] = crypto.pbkdf2Sync(data[1], salt,
+            1000, 64, `sha512`).toString(`hex`);
+            connection.query(`CALL Duomenu_Svieslente.sp_Create_Account('${ data[2]}', '${data[0]}' , '${data[1]}' , '${salt}')`, function(err, result){
+                if(err) throw err;
+                res.write(result[0][0].stat);
                 res.end();
-            }); //  data
-            
-          })
-          
+            })
+        })
     }
-    else if(req.url == '/secret'){
-        res.write("You found it!");
-        res.end();
-    }
-    else if(req.url == '/check_token'){
-        var status;
-        //console.log(req.headers['x-forwarded-for'] ||
-        //    req.socket.remoteAddress ||
-         //   null);
-        req.on('data', chunk => {
-            
-            var item = chunk.toString();
-            status = parser.find_token(item);
-            res.writeHead(200, {'Content-Type':'text/plain'});
-            res.write(JSON.stringify(status));
-            res.end();
-          })
-    }
-    else if(req.url == '/get_charts'){
-        var status;
-        //console.log(req.headers['x-forwarded-for'] ||
-        //    req.socket.remoteAddress ||
-         //   null);
-        req.on('data', chunk => { // id must be sent
-            
-            var item = chunk.toString();
-            var status = parser.get_charts(item);
-            res.writeHead(200, {'Content-Type':'text/plain'});
-            res.write(JSON.stringify(status));
-            res.end();
-          })
-    }
-    else{
-        res.writeHead(200, {'Content-Type':'text/plain'});
-        res.write("????");
-        res.end();
-    }
-    
-    
-    
-}).listen(port);
+    else res.end();
+};
+const server = http.createServer(requestListener);
+server.listen(port, host, ()=>{
+    connection.connect();
+    console.log(`Server running on ${host}:${port}`)
+})
